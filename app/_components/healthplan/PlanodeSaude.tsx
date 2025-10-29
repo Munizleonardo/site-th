ca5d7a4b8baed81c01c273f1101cb46909f7f83d
Leonardo Muniz <munizzleonardo@gmail.com>
2025-10-29 18:57:30 -0300

    fix: api

--- app/_components/healthplan/PlanodeSaude.tsx
+++ app/_components/healthplan/PlanodeSaude.tsx
@@ -28,7 +28,7 @@ export default function PlanodeSaude() {
     setIsLoading(true);
 
     try {
-      const res = await fetch("/api/planoesaude", {
+      const res = await fetch("/api/planodesaude", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
